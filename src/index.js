ef2fd30cb1f6fa9f983e63483e7aa0a8018786d8
DrewStock <andrewdstock@gmail.com>
2017-06-28 12:05:05 -0700

    course material covered: JSX, ES6 import statements, ReactDOM vs. React, component instances vs. component classes, render targets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+//ES6 import statements
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component that produces HTML. This defines a class of component, but doesn't instantiate an instance of the component.
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Original version, using function keyword
+// const App = function() {
+//     return <div>Hi!</div>;
+// }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Updated version, using ES6 fat arrow function
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+// Take this component's generated HTML and insert into the DOM. The first argument is the instantiated component and the second argument is a reference to the parent DOM element that is the target for the component to be rendered to.
+ReactDOM.render(<App />, document.querySelector('.container'));
