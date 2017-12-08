91b116fd3f1c70673a3b98fce1ab11e14a3a0128
DrewStock <andrewdstock@gmail.com>
2017-12-08 15:18:59 -0800

    restarting the tutorial, defined app and instantiated

--- src/index.js
+++ src/index.js
@@ -1,71 +1,17 @@
-//ES6 import statements
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import _ from 'lodash';
+// Create a new component. This component should produce some HTML
 
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail'
-
-const API_KEY = 'AIzaSyDDUSBbL_GcXiiOp7mXSUWAPWmYou1WEB8';
-
-// Create a new component that produces HTML. This defines a class of component, but doesn't instantiate an instance of the component.
-
-// Original version, using function keyword
+// ES5 syntax
 // const App = function() {
-//     return <div>Hi!</div>;
+//     return <div>Hi!</div>
 // }
 
-//Updated version, using ES6 fat arrow function
-// const App = () => {
-//     return (
-//         <div>
-//             <SearchBar />
-//         </div>
-//     );
-// }
-
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-             videos: [],
-             selectedVideo: null
-         };
-
-        this.videoSearch('kittens');
-    }
-
-    videoSearch(term) {
-        // YouTube API search
-        YTSearch({key: API_KEY, term: term}, (videos) => {
-        //ES6 feature - when the references to the key:value pair in the object being passed to this.setState are identical ( 'key'videos: 'value'videos ), the pair can be abbreviated to one string
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-             });
-        });
-    }
-
-    render() {
-
-        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
-
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch}/>
-                <VideoDetail video={this.state.selectedVideo}/>
-                <VideoList
-                 onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                 videos={this.state.videos} />
-            </div>
-        );
-    }
-
+// ES6 - fat arrow syntax
+const App = () => {
+    return <div>Hi!</div>
 }
 
-// Take this component's generated HTML and insert into the DOM. The first argument is the instantiated component and the second argument is a reference to the parent DOM element that is the target for the component to be rendered to.
+// Take this component's generated HTML and put it on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
